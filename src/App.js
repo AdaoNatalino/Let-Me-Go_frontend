5d3bae62fa10e39d7d09cbf39f5b06e06399c737
Adao Natalino <adaojosen@hotmail.com>
2020-07-16 14:14:09 +0100

    lunch

--- src/App.js
+++ src/App.js
@@ -24,6 +24,9 @@ function App() {
           <Route exact path="/profile">
             <Profile/>
           </Route>
+          <Route path="*">
+            <HomePage />
+          </Route>
         </Switch>
       </Router>
     </div>
