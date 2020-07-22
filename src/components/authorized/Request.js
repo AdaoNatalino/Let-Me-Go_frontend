8ef2320d08d2072fb0e321fa0ede1170b087609c
Adao Natalino <adaojosen@hotmail.com>
2020-07-22 16:58:51 +0100

    almost

--- src/components/authorized/Request.js
+++ src/components/authorized/Request.js
@@ -1,11 +1,45 @@
 import React from 'react'
 
-export default function Request () {
+import { Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
+import RequestCard from "./RequestCard"
+
+const useStyles = makeStyles({
+    gridContainer: {
+        paddingLeft: "20px",
+        paddingRight: "20px",
+        paddingTop: "20px"
+    }
+})
+
+export default function Request ({ user, trade }) {
+    const classes = useStyles();
+    const item1 = trade.item1
+    const item2 = trade.item2
+
+
+    const itemsInRequest = () => {
         return (
-            <div>
-                Hello from request
-            </div>
+            <>
+                <Grid key={item1.name} item xs={12} sm={6} md={6}>
+                    <RequestCard key={item1.id} item={item1} user={trade.user1}/>
+                </Grid> 
+                <Grid key={item2.name} item xs={12} sm={6} md={6}>
+                    <RequestCard key={item2.id} item={item2} user={trade.user2}/>
+                </Grid> 
+            </>
+        )
+    }
+        return (
+            <Grid 
+            container spacing={1} 
+            justify="center"
+            className={classes.gridContainer}>
+
+            { itemsInRequest() }
+
+            </Grid>
         )
  
 }
---
+++ src/components/authorized/RequestCard.js
@@ -0,0 +1,193 @@
+import React from 'react';
+import { useHistory } from "react-router-dom";
+
+
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import SwapVerticalCircleIcon from '@material-ui/icons/SwapVerticalCircle';
+import SaveIcon from '@material-ui/icons/Save';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+
+import API from "../../API"
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: red[400],
+  },
+}));
+
+export default function RequestCard( { item, user  } ) {
+  let history = useHistory();
+
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+  const [traded, setTraded] = React.useState(false);
+  const [item2_id, setItem2_id] = React.useState(null);
+
+  const item1_id = item.id
+  const tradeData = { item1_id, item2_id }
+
+  const handleTradeClick = () => {
+    API.requestNewTrade(tradeData)
+    history.push("/profile")
+  }
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  const handleTradingClick = () => {
+    setTraded(!traded);
+  }
+
+  return (
+    <Card className={classes.root}>
+      <CardHeader
+        // avatar={
+        //   <Avatar aria-label="recipe" className={classes.avatar}>
+        //     { item.category.title.charAt(0).toUpperCase() }
+        //   </Avatar>
+        // }
+        // action={
+        //   <IconButton aria-label="settings">
+        //     {/* <MoreVertIcon /> */}
+        //   </IconButton>
+        // }
+        title={ item.name }
+        subheader={`Condition: ${ item.condition.toUpperCase() }`}
+      />
+      <CardMedia
+        className={classes.media}
+        image={ item.image }
+        title="Paella dish"
+      />
+      {/* <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+         { item.description }
+        </Typography>
+      </CardContent> */}
+      <CardActions disableSpacing>
+{/* 
+        <IconButton 
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleTradingClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+          aria-label="add to favorites" color="secondary">
+
+          <SwapVerticalCircleIcon color="secondary" />
+        </IconButton>
+
+        <IconButton aria-label="share" color="primary">
+          <SaveIcon color="primary"/>
+        </IconButton> */}
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph style={{ color: 'red' }} >INFO:</Typography>
+          <Typography paragraph>
+            {item.description}
+          </Typography>
+          <Typography paragraph style={{ color: 'red' }}>OWNER:</Typography>
+          <Typography paragraph>
+            { user.name }
+          </Typography>
+          <Typography paragraph style={{ color: 'red' }}>CITY:</Typography>
+          <Typography paragraph>
+            { user.city }
+
+          </Typography>
+          <Typography>
+
+          </Typography>
+        </CardContent>
+      </Collapse>
+
+      {/* <Collapse in={traded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph style={{ color: 'red' }} >Which Item would you like to Swap?:</Typography>
+          <Grid item xs={12}>
+                <FormControl variant="outlined" className={classes.form}>
+                    <InputLabel htmlFor="outlined-category-native-simple">Item</InputLabel>
+                    <Select
+                    native
+                    onChange={ (e) => setItem2_id(e.target.value) }            
+                    label="Item"
+                    inputProps={{
+                        name: 'item',
+                        id: 'outlined-item-native-simple',
+                    }}
+                    > 
+                    <option aria-label="None" value="" />
+                    { itemsOptions() }
+                    </Select>
+                </FormControl>
+                { item2_id ? 
+          
+                  <Button
+                  onClick={handleTradeClick}
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  className={classes.submit}
+                  >
+                  Request a Trade!
+                  </Button>  
+
+                  : null}
+            </Grid>
+        </CardContent>
+      </Collapse> */}
+    </Card>
+  );
+}
