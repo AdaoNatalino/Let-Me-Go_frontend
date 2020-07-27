9464dfb42409a5e761608d4ee578a862fbb0f29b
Adao Natalino <adaojosen@hotmail.com>
2020-07-27 16:47:14 +0100

    share buttons

--- src/components/authorized/MyItemCard.js
+++ src/components/authorized/MyItemCard.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import { useHistory } from "react-router-dom";
 
-
 import {
-  EmailIcon,
-  FacebookIcon,
+  FacebookShareButton,
   FacebookMessengerIcon,
-  InstapaperIcon,
-  LineIcon,
-  LinkedinIcon,
-  LivejournalIcon,
-  RedditIcon,
-  TelegramIcon,
-  TumblrIcon,
+  FacebookMessengerShareButton,
   TwitterIcon,
+  TwitterShareButton,
   WhatsappIcon,
-} from "react-share"
+  WhatsappShareButton,
+  FacebookIcon,
+} from "react-share";
 
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
@@ -54,7 +49,6 @@ export default function MyItemCard({ item, setComponent }) {
     history.push("/profile")
   }
 
-
   const handleDeleteResp = (resp) => {
     if (resp.error) {
       alert(resp.error)
@@ -64,6 +58,9 @@ export default function MyItemCard({ item, setComponent }) {
     } 
   }
 
+  const shareUrl = 'http://github.com';
+  const title = 'GitHub';
+  
   return (
     <Card className={classes.root}>
       <CardActionArea>
@@ -73,33 +70,71 @@ export default function MyItemCard({ item, setComponent }) {
           title={item.name}
         />
         <CardContent>
-          {/* <Typography gutterBottom variant="h5" component="h2">
-            {item.name}
-          </Typography> */}
+          <Typography gutterBottom variant="h6" component="h6">
+          <IconButton aria-label="add to favorites" color="secondary">
+            <FacebookShareButton
+              url={shareUrl}
+              quote={title}
+              className="share-button"
+            >
+              <FacebookIcon size={23} round />
+            </FacebookShareButton>
+          </IconButton>
+
+          <IconButton aria-label="add to favorites" color="secondary">
+          {/* <ShareIcon style={{ color: "green" }} /> */}
+            <WhatsappShareButton
+              url={shareUrl}
+              title={title}
+              // separator=":: "
+              className="share-button"
+            >
+              <WhatsappIcon size={23} round />
+            </WhatsappShareButton>
+          </IconButton>
+
+          <IconButton aria-label="add to favorites" color="secondary">
+
+            <TwitterShareButton
+              url={shareUrl}
+              title={title}
+              className="share-button"
+            >
+              <TwitterIcon size={23} round />
+            </TwitterShareButton>
+          </IconButton>
+
+          </Typography>
+
           <Typography variant="body2" color="textSecondary" component="p">
             {item.condition}
           </Typography>
         </CardContent>
       </CardActionArea>
+
       <CardActions>
-
         <IconButton 
-
           onClick={() => { if(window.confirm('Delete the item?')) { handleDeleteItem() }; }}
-          aria-label="add to favorites" color="secondary">
+          aria-label="delete" color="secondary">
           <DeleteIcon color="secondary" />
         </IconButton>
 
-        <IconButton aria-label="add to favorites" color="secondary">
+        <IconButton aria-label="edit item" color="secondary">
+          {/* <Link color="primary" href={`/itemEdit/${item.id}`} >
+            <EditIcon color="primary" />
+          </Link> */}
+        </IconButton>
+        <IconButton aria-label="edit item" color="secondary">
+          {/* <Link color="primary" href={`/itemEdit/${item.id}`} >
+            <EditIcon color="primary" />
+          </Link> */}
+        </IconButton>
+
+       <IconButton aria-label="edit item" color="secondary">
           <Link color="primary" href={`/itemEdit/${item.id}`} >
             <EditIcon color="primary" />
           </Link>
-        </IconButton>
-
-        <IconButton aria-label="add to favorites" color="secondary">
-          {/* <ShareIcon style={{ color: "green" }} /> */}
-          <FacebookIcon/>
-        </IconButton>
+        </IconButton>          
 
       </CardActions>
     </Card>
